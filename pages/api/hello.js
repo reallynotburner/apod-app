a5a73f017eb3d62a08766a71ec50d06437f2acce
reallynotburner <reallynotburner@gmail.com>
2020-11-28 16:47:48 -0500

    improved the styling of the home page - removed scaling on hover - improved the portrait/mobile behavior - better alignment of header titles and swimlanes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+/*
+Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+  So darn nifty!
+*/
 export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
